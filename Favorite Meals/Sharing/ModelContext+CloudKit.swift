956d39bd1fc3597ae8e93eee3f782bac26af092c
Michael <MichealBoisJr@icloud.com>
2026-06-11 13:48:27 -0400

    Core feature is set up. Problems with acrtual sharing. Button does launch the builtin share correctly.

---
+++ Favorite Meals/Sharing/ModelContext+CloudKit.swift
@@ -0,0 +1,8 @@
+//
+//  ModelContext+CloudKit.swift
+//  Favorite Meals
+//
+//  Created by Michael Bois on 6/11/26.
+//
+
+import Foundation
---
+++ Favorite Meals/Sharing/UIViewControllerRepresentable.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewControllerRepresentable.swift
+//  Favorite Meals
+//
+//  Created by Michael Bois on 6/11/26.
+//
+
+import Foundation
